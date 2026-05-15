5e9c076b2a2e02faa57280b83af5d1dc081b4191
saqifalinks <saqifa.links@gmail.com>
2026-05-15 04:29:47 +0300

    Manual fix for vercel adapter and peer deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel/serverless'; // این خط را اضافه کن
 export default defineConfig({
   output: 'server',
+  adapter: vercel(), // این خط را اضافه کن
 });
